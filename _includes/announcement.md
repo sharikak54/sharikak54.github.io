5efadd8cb42a5df7e4c5861c752047dfff79b6c7
Shari Kuroyama <sharikak54@gmail.com>
2021-08-15 22:08:03 -0700

    save identifier work

--- _includes/announcement.md
+++ _includes/announcement.md
@@ -2,3 +2,8 @@
   "**Warning**: This site is a work in progress.  Proceed at your own risk!!!" 
   | markdownify 
 }}
+
+{{
+  "Unfortunately this site is not optimized for mobile.  Please use the desktop version for now."
+  | markdownify
+}}
---
+++ assets/js/identifier.js
@@ -0,0 +1,40 @@
+function onClickClicker(e) {
+  const x = e.offsetX - 100;
+  const y = e.offsetY - 100;
+  // console.log("(x=" + x + ",y=" + y + ")");
+
+  let section = 1;
+  let atan;
+  if (x === 0) {
+    section = y > 0 ? 0 : 4;
+  } else {
+    atan = Math.atan(y/x);
+    if (atan < 0) {
+      atan += Math.PI;
+    }
+    if (atan > line0) {
+      section = 5;
+    }
+  }
+  console.log("(x=" + x + ",y=" + y + ")", "atan:" + atan, "section:" + section);
+
+  console.log(section);
+  toggleColor(section);
+}
+
+function toggleColor(sectionNum) {
+  const $section = $("#clicker1").find("div.section.section" + sectionNum);
+  const was_white = $section.text() === "white";
+  $section.text(was_white ? "yellow" : "white")
+    .css("color", was_white ? "gold" : "");
+}
+
+const line0 = Math.atan(Math.PI/12);
+const line1 = Math.atan(5 * Math.PI/12);
+const line2 = Math.atan(7 * Math.PI/12);
+const line3 = Math.atan(11 * Math.PI/12);
+
+$(document).ready(() => {
+  $("#clicker1").click(onClickClicker);
+  console.log(line0, line1, line2, line3);
+});
