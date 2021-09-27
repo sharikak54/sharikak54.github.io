26d90e858100e020880d8b74dfd0ee52ace5c7a0
Shari Kuroyama <sharikak54@gmail.com>
2021-09-26 17:19:34 -0700

    mobile should mostly work now?

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,7 +1,27 @@
 $(document).ready(() => {
   $("#hamburger").click(onClickHamburger);
+  $(".mobileSubheader").click(onClickSubheader);
 });
 
 function onClickHamburger(e) {
-  $(this).parent().find("ul.mobileMenu").toggle();
+  $(this).parent().find("ul.mobileMenu")
+    .toggle(true)
+    .css("opacity", 1)
+    .css("pointer-events", "auto");
+}
+
+function onClickSubheader(e) {
+  const id = $(this).attr("id");
+  const $submenu = $(this).parent().find(`ul#submenu-${id}`);
+
+  // Toggle visibility of corresponding submenu
+  if (parseInt($submenu.css("opacity"))) {
+    $submenu.hide()
+      .css("opacity", 0)
+      .css("pointer-events", "none")
+  } else {
+    $submenu.show()
+      .css("opacity", 1)
+      .css("pointer-events", "auto")
+  }
 }
